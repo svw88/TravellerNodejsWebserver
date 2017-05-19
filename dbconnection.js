1efdfe559c25bbce074b4c2194395c1159eeb026
Stephanus van Wyk <svw88@hotmail.com>
2017-05-19 19:16:20 -0300

    changes

--- dbconnection.js
+++ dbconnection.js
@@ -1,10 +1,17 @@
 var mysql=require('mysql');
+var fs=require('fs');
 var connection=mysql.createPool({
 
-host:'localhost',
+host:'35.184.118.220',
+port:'3306',
 user:'root',
-password:'8888',
-database:'demo'
+password:'',
+database:'demo',
+ssl      : {
+        ca   : fs.readFileSync('./ssl/server-ca.pem'), // should be enough for AWS
+        key  : fs.readFileSync('./ssl/client-key.pem'), // required for google mysql cloud db
+        cert : fs.readFileSync('./ssl/client-cert.pem'), // required for google mysql cloud db
+  }
 
 
 });
