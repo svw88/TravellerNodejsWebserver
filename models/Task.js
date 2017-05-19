b5cb5f8151082c8a88b9f9eba1186ac98ce669ae
Stephanus van Wyk <svw88@hotmail.com>
2017-05-19 17:24:37 -0300

    node js web server

---
+++ models/Task.js
@@ -0,0 +1,58 @@
+var db = require('../dbconnection');
+
+var Task = {
+
+	getEvents : function(date, country, state, city, id, callback) {
+
+		return db.query("Select * from events where Date >= ? AND Country = ? AND State = ? AND City = ? AND Id > ?", [date, country, state, city, id], callback);
+
+	},
+	getUserEvents : function(date, alias, id, callback) {
+
+		return db.query("Select * from events Where Date >= ? AND Alias = ?  AND Id > ?", [date, alias, id], callback);
+
+	},
+	getSearchEvents : function(date, country, state, city, id, types, find, callback) {
+		find = "%"+find+"%";
+		return db.query("Select * from events where Date >= ? AND Country = ? AND State = ? AND City = ? AND Id > ? AND Type in (?) AND (Name like ? OR Description like ? OR Alias = ?)", [date, country, state, city, id, types, find, find, find], callback);
+
+	},
+	getMyEvents : function(userId, date, callback) {
+		return db.query("select events.Id,events.Name,events.Description,events.Type,events.Country,events.State,events.City,events.Addr,events.Site,events.Image,events.Date,events.Price,events.Currency,events.Alias from events join users  WHERE users.Id = ? AND events.Date >=?", [userId, date], callback);
+
+	},
+	createEvent : function(Task, callback) {
+		console.log("inside service");
+		console.log(Task);
+		return db.query("insert into events(Name,Description,Type,Country,State,City,Addr,Site,Image,Date,Price,Currency,Alias) values(?,?,?,?,?,?,?,?,?,?,?,?,?)", [Task.Name, Task.Description, Task.Type, Task.Country, Task.State, Task.City, Task.Addr, Task.Site, Task.Image, Task.Date, Task.Price, Task.Currency, Task.Alias], callback);
+	},
+	removeEvent : function(id, callback) {
+		return db.query("delete from events where Id=?", [id], callback);
+	},
+	createUser : function(Task, callback) {
+		console.log("inside service");
+		console.log(Task.Name);
+		return db.query("insert into users(Name,Surname,Alias,Email,Password) values(?,?,?,?,?)", [Task.Name, Task.Surname, Task.Alias, Task.Email, Task.Password], callback);
+	},
+	login : function(email, password, callback) {
+
+		return db.query("Select id,alias from users Where Email = ? AND Password = ?", [email, password], callback);
+
+	},
+	getCountries : function(callback) {
+
+		return db.query("Select id,name from countries", callback);
+
+	},
+	getStates : function(countryid, callback) {
+
+		return db.query("Select id,name from states where country_id = ?", [countryid], callback);
+
+	},
+	getCities : function(stateid, callback) {
+
+		return db.query("Select id,name from cities where state_id = ?", [stateid], callback);
+
+	}
+};
+module.exports = Task;
---
+++ routes/Tasks.js
@@ -0,0 +1,150 @@
+var express = require('express');
+var router = express.Router();
+var Task = require('../models/Task');
+
+router.get('/events/:date/:country/:state/:city/:id', function(req, res, next) {
+
+	Task.getEvents(req.params.date, req.params.country, req.params.state, req.params.city, req.params.id, function(err, rows) {
+
+		if (err) {
+			res.json(err);
+		} else {
+			res.json(rows);
+		}
+	});
+
+});
+
+router.get('/events/:date/:alias/:id', function(req, res, next) {
+
+	Task.getUserEvents(req.params.date, req.params.alias, req.params.id, function(err, rows) {
+
+		if (err) {
+			res.json(err);
+		} else {
+			res.json(rows);
+		}
+	});
+
+});
+
+router.get('/events/:date/:country/:state/:city/:id/:types/?:find', function(req, res, next) {
+
+	Task.getSearchEvents(req.params.date, req.params.country, req.params.state, req.params.city, req.params.id, req.params.types, req.params.find, function(err, rows) {
+
+		if (err) {
+			res.json(err);
+		} else {
+			res.json(rows);
+		}
+	});
+
+});
+
+router.get('/events/:userid/:date', function(req, res, next) {
+
+	Task.getMyEvents(req.params.userid, req.params.date, function(err, rows) {
+
+		if (err) {
+			res.json(err);
+		} else {
+			res.json(rows);
+		}
+	});
+
+});
+
+router.post('/create', function(req, res, next) {
+
+	Task.createEvent(req.body, function(err, count) {
+
+		//console.log(req.body);
+		if (err) {
+			res.json(err);
+		} else {
+			res.json(req.body);
+			//or return count for 1 & 0
+		}
+	});
+});
+
+router.delete('/remove/:id', function(req, res, next) {
+
+	Task.removeEvent(req.params.id, function(err, count) {
+
+		if (err) {
+			res.json(err);
+		} else {
+			res.json(count);
+		}
+
+	});
+});
+
+router.post('/register', function(req, res, next) {
+
+	Task.createUser(req.body, function(err, count) {
+
+		//console.log(req.body);
+		if (err) {
+			res.json(err);
+		} else {
+			res.json(req.body);
+			//or return count for 1 & 0
+		}
+	});
+});
+
+router.get('/login/:email/:password', function(req, res, next) {
+
+	Task.login(req.params.email, req.params.password, function(err, rows) {
+
+		if (err) {
+			res.json(err);
+		} else {
+			res.json(rows);
+		}
+	});
+
+});
+
+router.get('/countries', function(req, res, next) {
+
+	Task.getCountries(function(err, rows) {
+
+		if (err) {
+			res.json(err);
+		} else {
+			res.json(rows);
+		}
+	});
+
+});
+
+router.get('/states/:countryid', function(req, res, next) {
+
+	Task.getStates(req.params.countryid, function(err, rows) {
+
+		if (err) {
+			res.json(err);
+		} else {
+			res.json(rows);
+		}
+	});
+
+});
+
+router.get('/cities/:stateid', function(req, res, next) {
+
+	Task.getCities(req.params.stateid, function(err, rows) {
+
+		if (err) {
+			res.json(err);
+		} else {
+			res.json(rows);
+		}
+	});
+
+});
+
+module.exports = router;
