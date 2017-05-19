cb23febfa429106d7894d6723a5e7f0f908a2d38
Stephanus van Wyk <svw88@hotmail.com>
2017-05-19 20:35:48 -0300

    s

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 /*app.use('/resources',express.static(__dirname + '/images'));
 So now, you can use http://localhost:5000/resources/myImage.jpg to serve all the images instead of http://localhost:5000/images/myImage.jpg. */
 app.use('/', routes);
+app.use('/Tasks',Tasks);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
