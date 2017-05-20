6f175352aab420f1a56ef911e5a6a41d0d99e975
Stephanus van Wyk <svw88@hotmail.com>
2017-05-19 22:26:09 -0300

    s

--- dbconnection.js
+++ dbconnection.js
@@ -1,17 +1,14 @@
-var mysql=require('mysql');
-var fs=require('fs');
-var connection=mysql.createPool({
+var mysql = require('mysql');
+var connection = mysql.createPool({
 
-username: 'root',
- password: '',
- database: 'demo',
- host: 'localhost',
- dialect: 'mysql',
- dialectOptions: {
- socketPath: '/cloudsql/{projectName}:{zone}:{instance-name}'
- },
-
-
+	username : 'root',
+	password : '',
+	database : 'demo',
+	host : 'localhost',
+	dialect : 'mysql',
+	dialectOptions : {
+		socketPath : '/cloudsql/traveller-168120:us-central1:travellerdb'
+	},
 
 });
-module.exports=connection;
+module.exports = connection;
