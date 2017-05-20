2d21db9a0709563929c163e2b56c6e6a6de80251
Stephanus van Wyk <svw88@hotmail.com>
2017-05-19 22:36:24 -0300

    s

--- dbconnection.js
+++ dbconnection.js
@@ -4,7 +4,11 @@ var connection = mysql.createPool({
 	username : 'root',
 	password : '',
 	database : 'demo',
-	socketPath : '/cloudsql/traveller-168120:us-central1:travellerdb'
+	host : 'localhost',
+	dialect : 'mysql',
+	dialectOptions : {
+		socketPath : '/cloudsql/traveller-168120:us-central1:travellerdb'
+	},
 
 });
 module.exports = connection;
