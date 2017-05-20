629c3eb4a83935a79f0507b33d1ad57023a2765b
Stephanus van Wyk <svw88@hotmail.com>
2017-05-19 22:58:15 -0300

    s

--- dbconnection.js
+++ dbconnection.js
@@ -1,3 +1,10 @@
 var mysql = require('mysql');
-var connection = mysql.createPool('mysql://root:@/demo?unix_socket=/cloudsql/traveller-168120:us-central1:travellerdb');
+var connection = mysql.createPool({
+
+	user: 'root',
+	password : '',
+	database : 'demo',
+	socketPath : '/cloudsql/traveller-168120:us-central1:travellerdb'
+
+});
 module.exports = connection;
