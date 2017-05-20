253ac9b0e238329e4e2ce83751148a5884eec554
Stephanus van Wyk <svw88@hotmail.com>
2017-05-19 21:20:54 -0300

    s

--- dbconnection.js
+++ dbconnection.js
@@ -5,10 +5,8 @@ var connection=mysql.createPool({
  user: 'root',
  password: '',
  database: 'demo',
- dialect: 'mysql',
- dialectOptions: {
  socketPath: '/cloudsql/traveller-168120:us-central1:travellerdb'
- },
+
 
 
 
